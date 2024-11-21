7a30d782e76f83185a8789b6f8708967f90aa8d5
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2024-11-21 12:31:17 -0300

    Adição do mod fazendo à mão

---
+++ Forge/src/main/java/com/ntrwstudio/deathrium_collection/DeathriumCollection.java
@@ -0,0 +1,8 @@
+package com.ntrwstudio.deathrium_collection;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(DeathriumCollection.MODID)
+public class DeathriumCollection {
+    public static final String MODID = "deathrium_collection";
+}
