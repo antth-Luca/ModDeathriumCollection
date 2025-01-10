767b2e0c268b28cda4beb4bce83efaf1ae62f643
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2025-01-10 09:47:07 -0300

    Invulnerabilidade aos danos de queda e congelamento também!

--- neoforge-project/DeathriumCollection/src/main/java/io/github/antthluca/deathrium_collection/events/DCCommonEvents.java
+++ neoforge-project/DeathriumCollection/src/main/java/io/github/antthluca/deathrium_collection/events/DCCommonEvents.java
@@ -8,6 +8,7 @@ import net.minecraft.world.entity.player.Player;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.EntityInvulnerabilityCheckEvent;
+import net.neoforged.neoforge.event.entity.EntityTeleportEvent;
 import net.neoforged.neoforge.event.tick.PlayerTickEvent;
 
 @EventBusSubscriber(modid = DeathriumCollection.MODID)
@@ -25,10 +26,22 @@ public class DCCommonEvents {
     public static void onInvulnerabilityChecked(EntityInvulnerabilityCheckEvent event) {
         if (event.getEntity() instanceof ServerPlayer player && ArmorVerification.hasFullArmor(player)) {
             var source = event.getSource();
-            // Ivunerável ao dano de fogo
+            // Invulnerável ao dano de fogo
             if (source.is(DamageTypeTags.IS_FIRE)) event.setInvulnerable(true);
+            // Invulnerável ao dano de queda
+            if (source.is(DamageTypeTags.IS_FALL)) event.setInvulnerable(true);
+            // Invulnerável ao dano de congelamento
+            if (source.is(DamageTypeTags.IS_FREEZING)) event.setInvulnerable(true);
         }
     }
+
+    // Invulnerável ao dano de Ender Pearl
+    @SubscribeEvent
+	public static void onEnderPearlTeleport(EntityTeleportEvent.EnderPearl event) {
+		if (event.getEntity() instanceof ServerPlayer player && ArmorVerification.hasFullArmor(player)) {
+			event.setAttackDamage(0);
+		}
+	}
 
     // @SubscribeEvent
     // public static void onPotionApplicable(MobEffectEvent.Applicable event) {
