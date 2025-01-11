4d3b79773ac07bd2cbc8b096768aa8582f958b2c
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2025-01-11 13:52:33 -0300

    O capacete impede o enderman de ficar puto quando se olha nos olhos

--- neoforge-project/DeathriumCollection/src/main/java/io/github/antthluca/deathrium_collection/items/armor/DeathriumHelmet.java
+++ neoforge-project/DeathriumCollection/src/main/java/io/github/antthluca/deathrium_collection/items/armor/DeathriumHelmet.java
@@ -2,11 +2,19 @@ package io.github.antthluca.deathrium_collection.items.armor;
 
 import io.github.antthluca.deathrium_collection.items.custom.DeathriumArmor;
 import net.minecraft.core.Holder;
+import net.minecraft.world.entity.monster.EnderMan;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 
 public class DeathriumHelmet extends DeathriumArmor {
     public DeathriumHelmet(Holder<ArmorMaterial> material, Type type, Item.Properties prop) {
         super(material, type, prop);
+    }
+
+    @Override
+    public boolean isEnderMask(ItemStack stack, Player player, EnderMan endermanEntity) {
+        return true;
     }
 }
