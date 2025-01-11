1edb1115fae533f2febfa30d9c34c4735ebd19d1
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2025-01-11 14:10:38 -0300

    Os itens de Deathrium podem ser encantados!

--- neoforge-project/DeathriumCollection/src/main/java/io/github/antthluca/deathrium_collection/items/custom/DeathriumArmor.java
+++ neoforge-project/DeathriumCollection/src/main/java/io/github/antthluca/deathrium_collection/items/custom/DeathriumArmor.java
@@ -4,9 +4,20 @@ import net.minecraft.core.Holder;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 
 public class DeathriumArmor extends ArmorItem {
     public DeathriumArmor(Holder<ArmorMaterial> material, Type type, Item.Properties prop) {
         super(material, type, prop);
+    }
+
+    @Override
+    public boolean isBookEnchantable(ItemStack stack, ItemStack book) {
+        return true;
+    }
+
+    @Override
+    public boolean isEnchantable(ItemStack stack) {
+        return true;
     }
 }
