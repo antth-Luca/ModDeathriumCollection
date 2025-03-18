244835c94d2ee483a5b4fb833b68e1bbcac87f59
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2025-03-18 20:14:46 -0300

    Adição de sprites e itens (mas não terminados)

--- neoforge-project/DeathriumCollection/src/main/java/io/github/antthluca/deathrium_collection/integration/curios/init/InitRelicItems.java
+++ neoforge-project/DeathriumCollection/src/main/java/io/github/antthluca/deathrium_collection/integration/curios/init/InitRelicItems.java
@@ -12,6 +12,21 @@ public class InitRelicItems {
     public static final DeferredHolder<Item, Item> BLACK_HEART = RELIC_ITEMS.register(
         "black_heart", () -> new Item(new Item.Properties()));
 
+    public static final DeferredHolder<Item, Item> PASSAGE_KEY = RELIC_ITEMS.register(
+        "passage_key", () -> new Item(new Item.Properties()));
+
+    public static final DeferredHolder<Item, Item> LAST_WORDS_BELL = RELIC_ITEMS.register(
+        "last_words_bell", () -> new Item(new Item.Properties()));
+
+    public static final DeferredHolder<Item, Item> REAPER_FEATHER = RELIC_ITEMS.register(
+        "reaper_feather", () -> new Item(new Item.Properties()));
+
+    public static final DeferredHolder<Item, Item> DEATH_CUP = RELIC_ITEMS.register(
+        "death_cup", () -> new Item(new Item.Properties()));
+
+    public static final DeferredHolder<Item, Item> DEATH_RING = RELIC_ITEMS.register(
+        "death_ring", () -> new Item(new Item.Properties()));
+
     public static final DeferredHolder<Item, Item> LAMENTATION_CROWN = RELIC_ITEMS.register(
         "lamentation_crown", () -> new Item(new Item.Properties()));
 }
