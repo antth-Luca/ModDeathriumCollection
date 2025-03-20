fbb2cddb4e326695e04d60f3fdabe85e1ffeb6ee
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2025-03-19 22:15:53 -0300

    Efeito de relíquia Coração Negro
    * Conserto receita da Foice;

--- neoforge-project/DeathriumCollection/src/main/java/io/github/antthluca/deathrium_collection/integration/curios/handlers/DCRelicEffects.java
+++ neoforge-project/DeathriumCollection/src/main/java/io/github/antthluca/deathrium_collection/integration/curios/handlers/DCRelicEffects.java
@@ -15,39 +15,57 @@ import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
+import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
 import top.theillusivec4.curios.api.CuriosApi;
 
 @EventBusSubscriber(modid = DeathriumCollection.MODID)
 public class DCRelicEffects {
+    // Constants
     private static final Random RANDOM = new Random();
+    private static final float LIFESTEAL_BLACK_HEART = 0.3f;
+    private static final float CHANCE_DROP_RING = 0.15f;
 
+    // Events
     @SubscribeEvent
     public static void onEnemyDeath(LivingDeathEvent event) {
         if (event.getSource().getEntity() instanceof Player player) {
-
             // Verifica se o jogador tem a relíquia equipada
-            boolean hasRelic = hasCurio((LivingEntity) player, InitRelicItems.DEATH_RING.get());
+            boolean hasRing = hasCurio((LivingEntity) player, InitRelicItems.DEATH_RING.get());
 
-            if (hasRelic) {
-                // Sorteia a chance de drop (35%)
-                if (RANDOM.nextFloat() < 0.35f) {
-                    dropDeathriumBar(event.getEntity(), player);
+            if (hasRing) {
+                // Sorteia a chance de drop (15%)
+                if (RANDOM.nextFloat() < CHANCE_DROP_RING) {
+                    dropDeathriumIngot(event.getEntity(), player);
                 }
             }
         }
     }
 
+    @SubscribeEvent
+    public static void onPlayerDealDamage(LivingIncomingDamageEvent event) {
+        if (event.getSource().getEntity() instanceof Player player) {
+            // Verifica se o jogador tem a relíquia equipada
+            boolean hasHeart = hasCurio((LivingEntity) player, InitRelicItems.BLACK_HEART.get());
+
+            if (hasHeart) {
+                float healAmount = event.getAmount() * LIFESTEAL_BLACK_HEART;
+                player.heal(healAmount);
+            }
+        }
+    }
+
+    // Help functions
     @SuppressWarnings({ "deprecation", "removal" })
     public static boolean hasCurio(final LivingEntity entity, final Item curio) {
         final Optional<ImmutableTriple<String, Integer, ItemStack>> data = CuriosApi.getCuriosHelper().findEquippedCurio(curio, entity);
 		return data.isPresent();
 	}
 
-    private static void dropDeathriumBar(LivingEntity entity, Player player) {
+    private static void dropDeathriumIngot(LivingEntity entity, Player player) {
         // Cria o item de drop
-        ItemStack deathriumBar = new ItemStack(InitItems.DEATHRIUM_INGOT.get());
+        ItemStack deathriumIngot = new ItemStack(InitItems.DEATHRIUM_INGOT.get());
         
         // Solta o item no mundo na posição do inimigo derrotado
-        entity.spawnAtLocation(deathriumBar);
+        entity.spawnAtLocation(deathriumIngot);
     }
 }
